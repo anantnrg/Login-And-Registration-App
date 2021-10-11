28ec682706d09b4dce859701b237217edfcd376d
Anant Narayan <47025756+anantnrg@users.noreply.github.com>
2021-10-11 12:58:07 +0530

    Add files via upload

---
+++ LoginApp.py
@@ -0,0 +1,56 @@
+# Imports
+from tkinter import *
+from tkinter import messagebox
+import sqlite3
+
+# Make Window
+loginApp = Tk()
+loginApp.title("Login App")
+loginApp.geometry("340x150")
+loginApp.eval('tk::PlaceWindow . center')
+
+# Variables
+input_passwd = ""
+input_username = ""
+
+
+# Functions
+
+def check_credentials():
+    input_username = txtbox_username.get()
+    input_passwd = txtbox_password.get()
+    conn = sqlite3.connect('user_creds.db')
+    c = conn.cursor()
+    c.execute("SELECT usr_name FROM users WHERE usr_name =  ? AND usr_passwd = ?" ,(input_username,input_passwd))
+    data = c.fetchone()
+
+    if data is None:
+        messagebox.showerror("Incorrect Password", "You have entered an incorrect password. Please correct it.")
+    else:
+        messagebox.showinfo("Login successfull", "Your Login is successfull")
+        loginApp.destroy()
+
+
+    conn.commit()
+    conn.close()
+
+
+lbl_1 = Label(loginApp, text="")
+lbl_2 = Label(loginApp, text="")
+lbl_username = Label(loginApp, text="Username", padx=10, pady=5)
+lbl_password = Label(loginApp, text="Password", padx=10, pady=20)
+txtbox_username = Entry(loginApp, width=30)
+txtbox_password = Entry(loginApp, width=30, show="*")
+btn_login = Button(loginApp, text="Login", command=check_credentials)
+btn_cancel = Button(loginApp, text="Cancel")
+
+lbl_1.grid(row=0, column=0)
+lbl_2.grid(row=0, column=4)
+lbl_username.grid(row=3, column=1)
+lbl_password.grid(row=5, column=1)
+txtbox_username.grid(row=3, column=4)
+txtbox_password.grid(row=5, column=4)
+btn_cancel.grid(row=10, column=3)
+btn_login.grid(row=10, column=4)
+
+loginApp.mainloop()
---
+++ Register.py
@@ -0,0 +1,86 @@
+from tkinter import *
+import pickle
+import sqlite3
+import datetime
+
+# Make window
+
+regApp = Tk()
+regApp.title("Add New User")
+regApp.geometry("400x180")
+regApp.eval('tk::PlaceWindow . center')
+
+# variables  
+
+full_name = ""
+usr_name = ""
+usr_passwd = ""
+usr_passwd_confirm = ""
+
+### Functions
+# Save credentials
+global usr_id
+usr_id = datetime.datetime.now().strftime('%Y%m%d%H%M%S%f') 
+
+def save_credentials():
+	conn = sqlite3.connect('user_creds.db')
+	c = conn.cursor()
+	
+	c.execute("INSERT INTO users VALUES (:usr_code, :full_name,:usr_name, :usr_passwd)",
+			{
+				'usr_code' : usr_id,
+				'full_name' : txtbox_full_name.get(),
+				'usr_name' : txtbox_usr_name.get(),
+				'usr_passwd' : txtbox_usr_passwd.get()
+
+			})
+	conn.commit()
+	conn.close()
+
+
+
+def show_users():
+	conn = sqlite3.connect('user_creds.db')
+	c = conn.cursor()
+	c.execute("SELECT * FROM users")
+	data = c.fetchall()
+	print(data)
+	conn.commit()
+	conn.close()
+
+
+#### WIDGETS
+# Labels
+lbl_full_name = Label(regApp, text="Full Name", padx=60, pady=5)
+lbl_usr_name = Label(regApp, text="User Name", padx=60, pady=5)
+lbl_usr_passwd = Label(regApp, text="Password", padx=60, pady=5)
+lbl_usr_passwd_confirm = Label(regApp, text="Confirm Password", padx=60, pady=5)
+
+# Entries
+txtbox_full_name = Entry(regApp, width=20)
+txtbox_usr_name = Entry(regApp, width=20)
+txtbox_usr_passwd = Entry(regApp, width=20, show="*")
+txtbox_usr_passwd_confirm = Entry(regApp, width=20, show="*")
+
+# Buttons
+btn_ok = Button(regApp, text="Save", pady=10, padx=20, width=10, command=save_credentials)
+btn_show_users = Button(regApp, text="Show users", pady=10, padx=20, width=10, command=show_users)
+
+
+
+# Shove it on the screen
+lbl_full_name.grid(column=2, row=2)
+lbl_usr_name.grid(column=2, row=4)
+lbl_usr_passwd.grid(column=2, row=6)
+lbl_usr_passwd_confirm.grid(column=2, row=8)
+
+txtbox_full_name.grid(column=6, row=2)
+txtbox_usr_name.grid(column=6, row=4)
+txtbox_usr_passwd.grid(column=6, row=6)
+txtbox_usr_passwd_confirm.grid(column=6, row=8)
+
+btn_ok.grid(column=6, row=10)
+btn_show_users.grid(column=2, row=10)
+
+
+regApp.mainloop()
